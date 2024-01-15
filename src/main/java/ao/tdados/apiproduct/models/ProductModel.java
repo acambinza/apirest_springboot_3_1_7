92b0c538b9f69a95b43eb2308b43865b957b846b
Anselmo Cambinza <acambinza@cetim.ms>
2024-01-15 14:02:03 +0100

    feat:CRUD products done

--- src/main/java/ao/tdados/apiproduct/models/ProductModel.java
+++ src/main/java/ao/tdados/apiproduct/models/ProductModel.java
@@ -1,6 +1,7 @@
 package ao.tdados.apiproduct.models;
 
 import jakarta.persistence.*;
+import org.springframework.hateoas.RepresentationModel;
 
 
 import java.io.Serializable;
@@ -10,7 +11,7 @@ import java.util.UUID;
 
 @Entity
 @Table(name= "TB_PRODUCTS")
-public class ProductModel implements Serializable {
+public class ProductModel extends RepresentationModel<ProductModel> implements Serializable {
    private static final long serialVersionUUID = 1L;
 
    @Id
